b6e808348aa3187d3207bc450887654dfcf936e4
ZachepaRostislav <zachepa.rostislav@gmail.com>
2023-02-20 21:31:18 +0200

    finish hw 7

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+function createMarkup(array) {
+  return array.map(({ preview, original, description }) => {
+    return `
+    <li>
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>    
+    `
+  }).join('')
+}
+
+const markup = createMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
